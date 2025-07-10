f4425f1a64f0275e55f4895263dd3c2afac9b868
ciheaturu <40758176+ciheaturu@users.noreply.github.com>
2025-07-10 18:21:12 +0000

    Deploying to gh-pages from @ ciheaturu/ciheaturu.github.io@17e3f68ddcfcf79271a23950585b11f42a1e5553 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -355,6 +355,17 @@ ninja.data = [{
             window.location.href = "/blog/2015/math/";
           
         },
+      },{id: "post-a-post-with-code",
+        
+          title: "a post with code",
+        
+        description: "an example of a blog post with some code",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/code/";
+          
+        },
       },{id: "books-remote-sensing-and-gis-for-ecologists",
           title: 'Remote Sensing and GIS for Ecologists',
           description: "",
